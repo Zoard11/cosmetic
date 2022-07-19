8cdb67f13ec711401a96bfcdc6eaa1a9b32a551b
Parajdi Zoard <parajdi.zoard@gmail.com>
2022-07-19 18:49:31 +0300

    getInformation

--- Services.js
+++ Services.js
@@ -1,7 +1,7 @@
 import {ipAddress} from './constants';
 
 export function getInformation(name) {
-  return fetch(`${ipAddress}/api/getInformation/${name}`, {
+  return fetch(`${ipAddress}/api/${name}`, {
     method: 'GET',
   })
     .then(response => response.json())
